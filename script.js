070ea211b2c55e2cd672aedc145e4797ef780dc4
Maks <maksymilian.lewinski@gmail.com>
2021-12-04 18:27:28 +0100

    Working!!

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let button = document.getElementById("enter");
 let input = document.getElementById("userInput");
 let ul = document.querySelector("ul");
-button.addEventListener("click",function () {
+
+function addListItem () {
+    if (input.value.length > 0){
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-} )
+    input.value = "";
+    }
+}
+button.addEventListener("click", addListItem);
+input.addEventListener("keypress", function(event){
+    if (event.key==='Enter')
+    addListItem();
+
+
+})
